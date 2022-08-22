773d3930ea3e61b80964fbfc165a40e3f8e8c279
Eric Zhang <56938909+theericzhang@users.noreply.github.com>
2022-08-22 16:01:19 -0400

    Pull the next 5 tweets and relevant metadata
    SERVER
    
    create a new TwitterApi client using a bearer token
    
    set client to read only
    
    create a new user object with usersByUsernames, then get the id of that user & set it to myTwitterId
    
    With this new Id property, we are able to GET the last 5 tweets from the user with the userTimeline function built into v2.
    
    Setting the max results to 5, we limit it to 5.

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 import { TwitterApi } from "twitter-api-v2";
-import 'dotenv/config';
+import "dotenv/config";
 
-console.log("server started")
+console.log("server started");
 
-const clientMain = new TwitterApi(process.env.BEARER_TOKEN)
+const clientMain = new TwitterApi(process.env.BEARER_TOKEN);
 
 // const clientMain = new TwitterApi(
 //     {
@@ -16,7 +16,7 @@ const roClient = clientMain.readOnly;
 // const myTimeline = await roClient.v1.homeTimeline();
 // console.log(myTimeline.tweets.length, 'fetched.');
 
-const user = await roClient.v2.usersByUsernames('anericzhang')
+const user = await roClient.v2.usersByUsernames("anericzhang");
 // console.log("success", user);
 
 const myTwitterId = user?.data[0]?.id;
@@ -26,19 +26,37 @@ console.log(myTwitterId);
 
 const myTimeline = await roClient.v2.userTimeline(myTwitterId, {
     max_results: 5,
-    expansions: ['attachments.media_keys', 'attachments.poll_ids', 'referenced_tweets.id'],
-    'media.fields': ['url'],
-  });
-  
-  // myTimeline.includes contains a TwitterV2IncludesHelper instance
-  // how can i change this to display only 5 tweets?
+    expansions: [
+        "attachments.media_keys",
+        "attachments.poll_ids",
+        "referenced_tweets.id",
+    ],
+    "media.fields": ["url"],
+});
 
-console.log("logging data: ", myTimeline.data)
+// myTimeline.includes contains a TwitterV2IncludesHelper instance
+// how can i change this to display only 5 tweets?
+
+// console.log("logging data: ", myTimeline.data);
+let i = 0;
+for await (const tweet of myTimeline) {
+    console.log("the tweet", tweet);
+    // returns 5 tweet objects from reverse chron order. 
+    // if the tweets include a referenced tweet (e.g. a retweet)
+    // i need to somehow indicate that those tweets were retweeted.
+    // if tweet.referenced_tweets != null {include logo rendering} (from the client side, at least)
+
+    const medias = myTimeline.includes.medias(tweet);
+    console.log("medias is: ", medias);
+    i++;
+    if (i === 5) break;
+}
+
 //   for await (const tweet of myTimeline) {
 //     console.log("the tweet: ", tweet);
 //     const medias = myTimeline.includes.medias(tweet);
 //     const poll = myTimeline.includes.poll(tweet);
-  
+
 //     if (medias.length) {
 //       console.log('This tweet contains medias! URLs:', medias.map(m => m.url));
 //     }
@@ -46,4 +64,3 @@ console.log("logging data: ", myTimeline.data)
 //       console.log('This tweet contains a poll! Options:', poll.options.map(opt => opt.label));
 //     }
 //   }
-
