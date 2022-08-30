cb4b5dbeb87a862c871f02c23d2e2893ff1262b5
Eric Zhang <56938909+theericzhang@users.noreply.github.com>
2022-08-30 18:00:17 -0400

    hover states implemented, background color of hovered tweets

--- client/suspense-twitter-client/src/components/Wrapper.jsx
+++ client/suspense-twitter-client/src/components/Wrapper.jsx
@@ -18,12 +18,15 @@ export default function Wrapper() {
     const tweetDataArray = tweetData?.myTimelineTweetDataObject?.tweetsDataArray;
     const myUserProfileData = tweetData?.myTimelineTweetDataObject?.myUserProfileData;
     const tweetComponentArray = tweetDataArray?.map((tweetData, index) => {
-        return <Tweet text={tweetData.text}
-                      displayName={myUserProfileData?.data[0]?.name}
-                      username={myUserProfileData?.data[0]?.username}
-                      profile_image_url={myUserProfileData?.data[0]?.profile_image_url} 
-                      key={tweetData + index} 
-                />
+        return (
+            <Tweet text={tweetData.text}
+                   displayName={myUserProfileData?.data[0]?.name}
+                   username={myUserProfileData?.data[0]?.username}
+                   profile_image_url={myUserProfileData?.data[0]?.profile_image_url} 
+                   key={tweetData + index} 
+                   isLastTweet={index === tweetDataArray?.length - 1}
+            />
+        )
     })
 
     console.log(tweetComponentArray);
