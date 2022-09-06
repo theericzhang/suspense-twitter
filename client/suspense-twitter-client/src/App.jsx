777f173018282c35e6a5d1fab3b302ae16ff9c92
Eric Zhang <56938909+theericzhang@users.noreply.github.com>
2022-09-06 14:58:21 -0400

    Created Dark Mode styles from Twitter's 'Dim' mode. Activating through context api & conditional rendering

--- client/suspense-twitter-client/src/App.jsx
+++ client/suspense-twitter-client/src/App.jsx
@@ -1,15 +1,33 @@
-import { Suspense } from "react";
+import { Suspense, useState, useEffect, createContext } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import AppFrame from "./components/AppFrame";
 
-// export const TweetDataContext = createContext();
+export const ColorSchemeContext = createContext();
 
 function App() {
+
+    const [ colorScheme, setColorScheme ] = useState('');
+
+    // using window events to detect color scheme set by user
+    function detectColorScheme () {
+        if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            setColorScheme('dark');
+        } else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches) {
+            setColorScheme('light');
+        }
+    }
+
+    useEffect(() => {
+        detectColorScheme();
+    }, []);
+
     return (
-        <div className="App">
-            <AppFrame />
-        </div>
+        <ColorSchemeContext.Provider value={colorScheme}>
+            <div className="App" id={colorScheme === 'light' ? '' : 'dark'}>
+                <AppFrame />
+            </div>
+        </ColorSchemeContext.Provider>
     );
 }
 
