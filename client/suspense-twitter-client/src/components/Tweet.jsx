791938655c4f81d9905a156ab2132f3bc0d3d98a
Eric Zhang <56938909+theericzhang@users.noreply.github.com>
2022-09-08 17:54:55 -0400

    Replacing RT'd data with original tweet.
    Using the read-only twitter client and the tweet id from the RT'd tweet, grab that tweet information including its media and author data.
    
    Using conditional rendering in Wrapper.jsx, decide if it is suitable to hydrate the `<Tweet />` component with `myUserProfileData` or `referencedTweet` data

--- client/suspense-twitter-client/src/components/Tweet.jsx
+++ client/suspense-twitter-client/src/components/Tweet.jsx
@@ -2,6 +2,8 @@ import { React, useContext } from "react";
 import { ColorSchemeContext } from "../App";
 
 export default function Tweet( { text,
+                                 type,
+                                 timelineOwnerDisplayName,
                                  displayName,
                                  username,
                                  profile_image_url,
@@ -143,16 +145,24 @@ export default function Tweet( { text,
                 return (n / ranges[i].divider).toFixed(1).toString() + ranges[i].suffix;
             }
         }
-        return n.toString();
+        return n?.toString();
     }
 
     return (
         <>
             <div className="tweet-wrapper" id={colorScheme === 'light' ? '' : 'dark-tweet-wrapper'}>
                 <div className="tweet-avatar-column">
+                    {type === 'retweeted' && <div className="tweet-reference-symbol">
+                                                <svg viewBox="0 0 24 24" aria-hidden="true" className="retweet-reference" fill={colorScheme === 'light' ? "#536471" : "#8B98A5"}><g><path d="M23.615 15.477c-.47-.47-1.23-.47-1.697 0l-1.326 1.326V7.4c0-2.178-1.772-3.95-3.95-3.95h-5.2c-.663 0-1.2.538-1.2 1.2s.537 1.2 1.2 1.2h5.2c.854 0 1.55.695 1.55 1.55v9.403l-1.326-1.326c-.47-.47-1.23-.47-1.697 0s-.47 1.23 0 1.697l3.374 3.375c.234.233.542.35.85.35s.613-.116.848-.35l3.375-3.376c.467-.47.467-1.23-.002-1.697zM12.562 18.5h-5.2c-.854 0-1.55-.695-1.55-1.55V7.547l1.326 1.326c.234.235.542.352.848.352s.614-.117.85-.352c.468-.47.468-1.23 0-1.697L5.46 3.8c-.47-.468-1.23-.468-1.697 0L.388 7.177c-.47.47-.47 1.23 0 1.697s1.23.47 1.697 0L3.41 7.547v9.403c0 2.178 1.773 3.95 3.95 3.95h5.2c.664 0 1.2-.538 1.2-1.2s-.535-1.2-1.198-1.2z"></path></g></svg>
+                                            </div>
+                    }
                     <img src={`${profile_image_url}`} alt="" className="user-avatar" />
                 </div>
                 <div className="tweet-body">
+                    {type === 'retweeted' && <div className="tweet-reference-user">
+                                                <h5 className="tweet-reference-user-action" id={colorScheme === 'light' ? '' : 'tweet-reference-user-action-dark'}>{timelineOwnerDisplayName} retweeted</h5>
+                                             </div>
+                    }
                     <div className="tweet-meta-info">
                         <h4 className="display-name" id={colorScheme === 'light' ? '' : 'dark-display-name'}>{displayName}</h4>
                         {isVerified && <svg viewBox="0 0 24 24" className="verified-account-logo" fill={colorScheme === 'light' ? "rgb(29, 155, 240)" : "rgb(247, 249, 249)"}>
