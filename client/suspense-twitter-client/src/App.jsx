8be94204ee4098aef1ba42f73941e01e9348b0f7
Eric Zhang <56938909+theericzhang@users.noreply.github.com>
2022-09-12 17:39:51 -0400

    successfully performed data fetching at the top level (App.jsx) and pass Tweet components as context

--- client/suspense-twitter-client/src/App.jsx
+++ client/suspense-twitter-client/src/App.jsx
@@ -3,8 +3,11 @@ import "./App.css";
 import AppFrame from "./components/AppFrame";
 import Toggle from "./components/Toggle";
 import SearchBar from "./components/SearchBar";
+import Tweet from "./components/Tweet";
 
 export const ColorSchemeContext = createContext();
+export const TweetComponentArrayContext = createContext();
+export const ErrorMessageContext = createContext();
 
 function App() {
 
@@ -34,11 +37,99 @@ function App() {
         document.body.style.backgroundColor = "#FFFFFF";
     }
 
+    // data
+
+    const [ tweetData, setTweetData ] = useState({});
+    const [ errorMessage, setErrorMessage ] = useState(null);
+    
+    async function fetchTweetData() {
+        const res = await fetch('http://localhost:5000/tweets');
+        if (!res.ok) {
+            console.log(res); // returns response with status code
+            const responseError = await res.json();
+            // returns server side generated error, e.g. 'could not find user'
+            setErrorMessage(await responseError);
+        }
+        const data = await res.json();
+        setTweetData(data);
+        console.log(data);
+
+        // try catchh block attempt at error handling
+        // erroneous behavior - breaks rule of hooks. caught error will display rule of hooks error
+
+        // try {
+        //     const res = await fetch('http://localhost:5000/tweets');
+        //     if (!res.ok) {
+        //         console.log(res); // returns response with status code
+        //         const responseError = await res.json();
+        //         // returns server side generated error, e.g. 'could not find user'
+        //         setErrorMessage(await responseError);
+        //         throw new Error(responseError);
+        //     }
+        //     const data = await res.json();
+        //     setTweetData(data);
+        //     console.log(data);
+        // } catch (error) {
+        //     console.error(error);
+        // }
+
+
+        // fetch('http://localhost:5000/tweets')
+        // .then((res) => {
+        //     if (!res.ok) {
+        //         const responseError = res.json();
+        //         setErrorMessage(responseError);
+        //         throw new Error('responseError');
+        //     }
+        //     const data = res.json();
+        //     setTweetData(data);
+        // }).catch(err => {
+        //     console.error(err);
+        // })
+
+    }
+
+    useEffect(() => {
+        fetchTweetData();
+        // To fetch again after data has been updated by user in search bar,
+        // try passing the fetchTweetData(); function as a prop to the search bar class
+        // then call the function after the post request has been made
+    }, []);
+
+
+    const currentDate = new Date();
+    const currentTimeSinceEpochInms = currentDate.getTime();
+    // console.log(currentTimeSinceEpochInms);
+    const tweetDataArray = tweetData?.myTimelineTweetDataObject?.tweetsDataArray;
+    const myUserProfileData = tweetData?.myTimelineTweetDataObject?.myUserProfileData;
+    const tweetComponentArray = tweetDataArray?.map((tweetData, index) => {
+        return (
+            <Tweet text={tweetData?.tweet?.text}
+                   type={tweetData?.type}
+                   timelineOwnerDisplayName={myUserProfileData?.data[0]?.name}
+                   displayName={tweetData?.hasOwnProperty("author") ? tweetData?.author[0]?.name : myUserProfileData?.data[0]?.name}
+                   username={tweetData?.hasOwnProperty("author") ? tweetData?.author[0]?.username : myUserProfileData?.data[0]?.username}
+                   profile_image_url={tweetData?.hasOwnProperty("author") ? tweetData?.author[0]?.profile_image_url : myUserProfileData?.data[0]?.profile_image_url}
+                   isVerified={tweetData?.hasOwnProperty("author") ? tweetData?.author[0]?.verified : myUserProfileData?.data[0]?.verified}
+                   public_metrics={tweetData?.tweet?.public_metrics} 
+                   time_created={tweetData?.tweet?.created_at}
+                   currentTimeSinceEpochInms={currentTimeSinceEpochInms}
+                   mediaArray={tweetData?.media}
+                   isLastTweet={index === tweetDataArray?.length - 1}
+                   key={tweetData?.tweet + index} 
+            />
+        )
+    })
+
     return (
         <ColorSchemeContext.Provider value={colorScheme}>
             <div className="App" id={colorScheme === 'light' ? '' : 'dark'}>
                 <div className="sidebar-column-left"></div>
-                <AppFrame />
+                <TweetComponentArrayContext.Provider value={tweetComponentArray}>
+                    <ErrorMessageContext.Provider value={errorMessage}>
+                        <AppFrame />
+                    </ErrorMessageContext.Provider>
+                </TweetComponentArrayContext.Provider>
                 <div className="sidebar-column-right">
                     <Toggle colorScheme={colorScheme} 
                             setColorScheme={setColorScheme}
--- client/suspense-twitter-client/src/components/Wrapper.jsx
+++ client/suspense-twitter-client/src/components/Wrapper.jsx
@@ -2,91 +2,13 @@ import { React, useState, useEffect, useContext } from "react";
 import Tweet from "./Tweet";
 import Error from "./Error";
 import { ColorSchemeContext } from "../App";
+import { TweetComponentArrayContext } from "../App";
+import { ErrorMessageContext } from "../App";
 
 export default function Wrapper() {
-    const [ tweetData, setTweetData ] = useState({});
-    const [ errorMessage, setErrorMessage ] = useState(null);
-    
-    async function fetchTweetData() {
-        const res = await fetch('http://localhost:5000/tweets');
-        if (!res.ok) {
-            console.log(res); // returns response with status code
-            const responseError = await res.json();
-            // returns server side generated error, e.g. 'could not find user'
-            setErrorMessage(await responseError);
-        }
-        const data = await res.json();
-        setTweetData(data);
-        console.log(data);
-
-        // try catchh block attempt at error handling
-        // erroneous behavior - breaks rule of hooks. caught error will display rule of hooks error
-
-        // try {
-        //     const res = await fetch('http://localhost:5000/tweets');
-        //     if (!res.ok) {
-        //         console.log(res); // returns response with status code
-        //         const responseError = await res.json();
-        //         // returns server side generated error, e.g. 'could not find user'
-        //         setErrorMessage(await responseError);
-        //         throw new Error(responseError);
-        //     }
-        //     const data = await res.json();
-        //     setTweetData(data);
-        //     console.log(data);
-        // } catch (error) {
-        //     console.error(error);
-        // }
-
-
-        // fetch('http://localhost:5000/tweets')
-        // .then((res) => {
-        //     if (!res.ok) {
-        //         const responseError = res.json();
-        //         setErrorMessage(responseError);
-        //         throw new Error('responseError');
-        //     }
-        //     const data = res.json();
-        //     setTweetData(data);
-        // }).catch(err => {
-        //     console.error(err);
-        // })
-
-    }
-
-    useEffect(() => {
-        fetchTweetData();
-        // To fetch again after data has been updated by user in search bar,
-        // try passing the fetchTweetData(); function as a prop to the search bar class
-        // then call the function after the post request has been made
-    }, []);
-
     const colorScheme = useContext(ColorSchemeContext);
-
-    const currentDate = new Date();
-    const currentTimeSinceEpochInms = currentDate.getTime();
-    // console.log(currentTimeSinceEpochInms);
-    const tweetDataArray = tweetData?.myTimelineTweetDataObject?.tweetsDataArray;
-    const myUserProfileData = tweetData?.myTimelineTweetDataObject?.myUserProfileData;
-    const tweetComponentArray = tweetDataArray?.map((tweetData, index) => {
-        return (
-            <Tweet text={tweetData?.tweet?.text}
-                   type={tweetData?.type}
-                   timelineOwnerDisplayName={myUserProfileData?.data[0]?.name}
-                   displayName={tweetData?.hasOwnProperty("author") ? tweetData?.author[0]?.name : myUserProfileData?.data[0]?.name}
-                   username={tweetData?.hasOwnProperty("author") ? tweetData?.author[0]?.username : myUserProfileData?.data[0]?.username}
-                   profile_image_url={tweetData?.hasOwnProperty("author") ? tweetData?.author[0]?.profile_image_url : myUserProfileData?.data[0]?.profile_image_url}
-                   isVerified={tweetData?.hasOwnProperty("author") ? tweetData?.author[0]?.verified : myUserProfileData?.data[0]?.verified}
-                   public_metrics={tweetData?.tweet?.public_metrics} 
-                   time_created={tweetData?.tweet?.created_at}
-                   currentTimeSinceEpochInms={currentTimeSinceEpochInms}
-                   mediaArray={tweetData?.media}
-                   isLastTweet={index === tweetDataArray?.length - 1}
-                   key={tweetData?.tweet + index} 
-            />
-        )
-    })
-
+    const tweetComponentArray = useContext(TweetComponentArrayContext);
+    const errorMessage = useContext(ErrorMessageContext);
     console.log(tweetComponentArray);
     
     return (
